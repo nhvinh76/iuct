f377aec643be683f40755d64d4ab9749b67b2a12
nhvinh76 <91956279+nhvinh76@users.noreply.github.com>
2022-10-20 15:55:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello Cẩm Tiên iu zấu!",
+  introDesc: `Nhân dịp lễ như lày \n
+    Anh có mụt món quà muốn tặng cho em\n
+    Hãy nhận lấyyyyy :>`,
+  btnIntro: "hihi",
+  title: "Sau đây là màng bốc thăm trúng thưởng :>",
+  desc: "Cẩm Tiên có iu anh hok ",
+  btnYes: "Vẫn cứ là iu anh <33",
+  btnNo: "Không, anh bị đin à :>",
+  question: `Gòi sao Cẩm Tiên lại iu anh zọ <3  \n Muốn nói gì thì gửi cho anhhhh`,
+  btnReply: "Gửi cho anh :>",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Sời không cần nói anh cũng biếttt :>",  
+  messDesc: `Chúc mừng em đã nhận được một món quà :>\n
+             Đó là một cái thơm má từ anh `,
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://m.me/8464z", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};  
